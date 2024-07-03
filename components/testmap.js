0dd98d653efc7d4815a157f335e1e97dfc1a2c46
neptune2716 <cyril.hup2716@gmail.com>
2024-07-04 01:59:00 +0200

    deploiment10

--- components/testmap.js
+++ components/testmap.js
@@ -394,10 +394,7 @@ const TestMap = () => {
             mapRef.current = mapInstance;
           }}
         >
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          />
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
           <MapComponent
             setBounds={setBounds}
             fetchPointsWithinBounds={fetchPointsWithinBounds}
